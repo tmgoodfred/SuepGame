ccc462c38fac527ab685868793907efc2cae1314
Tyler <68490241+tmgoodfred@users.noreply.github.com>
2022-04-25 17:31:58 -0500

    Update Form1.cs
    added comments

--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,7 @@ namespace SuepLeviGame
         String[] leviTalk = { "Oh... Who are you?", "", "...Poop Time?", "", "Oh, I'm sorry dear.", "Did you know...", "That I love you?", "", "" };
         String[] suepTalk = { "", "I'm SuepTime!", "", "What? No! SUEP TIME", "", "", "", "YOU DO?", "That's kinda cringe bro." };
 
-        int buttonPressCount = 0;
+        int buttonPressCount = 0;   //keeps track of where the dialogue is
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -28,18 +28,18 @@ namespace SuepLeviGame
             {
                 leviTxt.Text = (leviTalk[buttonPressCount]);
                 suepTxt.Text = (suepTalk[buttonPressCount]);
-                if (buttonPressCount <= 9)
+                if (buttonPressCount <= 9)  //hard coded based on the amount of dialogue. Not optimal, but it'll work
                 {
-                    buttonPressCount += 1;
+                    buttonPressCount += 1;  //if you press the button it moves forward 1 piece of dialogue
                 }
                 else
                 {
-                    Console.WriteLine("Nope");
+                    Console.WriteLine("Nope"); //if you exceed the dialogue
                 }
             }
             catch
             {
-                Console.WriteLine("oops");
+                Console.WriteLine("oops"); //if you got out of bounds of the array
             }
         }
 
@@ -49,18 +49,18 @@ namespace SuepLeviGame
             {
                 if (buttonPressCount > 0)
                 {
-                    buttonPressCount -= 1;
+                    buttonPressCount -= 1;  //steps back one part in dialogue
                 }
                 else
                 {
-                    Console.WriteLine("Nope");
+                    Console.WriteLine("Nope");  //if you exceed the dialogue
                 }
                 leviTxt.Text = (leviTalk[buttonPressCount]);
                 suepTxt.Text = (suepTalk[buttonPressCount]);
             }
             catch
             {
-                Console.WriteLine("oops");
+                Console.WriteLine("oops");  //if you got out of bounds of the array
             }
         }
 
